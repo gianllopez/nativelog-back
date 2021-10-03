aea9ab92b41ad5c3da11d11f6d0e287893068cbd
Gian López <lopezarizagianlucas@gmail.com>
2021-10-03 16:08:09 -0500

    master: ready for code review.

--- src/services/users.js
+++ src/services/users.js
@@ -1,24 +1,26 @@
 const mongoose = require('mongoose');
+const jwt = require('jsonwebtoken');
 const { UserModel } = require('../models/User');
 
 class UsersService {
 
-  #col = null;
-
   constructor() {
     mongoose.connect('mongodb://localhost:27017/nativelog')
-      .then(db => this.#col = db.connection.collection('users'))
       .catch(err => console.error(err));
   };
 
-  async createUser(data) {
+  async create(data) {
     return new Promise((res, rej) => {
       let user = new UserModel(data);
       user.save(err => {
-        if (err) rej(err);
-        res(user);
+        if (err) return rej(err);
+        res(user);        
       });
     });
+  };
+
+  async get(creds) {
+    return await UserModel.findOne(creds).exec();
   };
 
 };
